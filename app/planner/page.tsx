e95907a06c98fde18174d531ee5135f1a0e17932
vermavijay5670-cmd <vermavijay5670@gmail.com>
2026-08-18 10:25:25 +0530

    kinetic grid add to planner

--- app/planner/page.tsx
+++ app/planner/page.tsx
@@ -1,16 +1,13 @@
 import { PageShell } from "@/components/ui/PageShell";
 import { PlannerSection } from "@/components/planner/PlannerSection";
-import MovingGrid from "@/components/ui/kinetic-grid";
+import KineticGrid from "@/components/ui/kinetic-grid";
 
 export default function PlannerPage() {
   return (
-    <>
-      <div className="fixed inset-0 -z-10 overflow-hidden">
-        <MovingGrid showTitle={false} forceDark />
-      </div>
-      <PageShell>
-        <PlannerSection />
-      </PageShell>
-    </>
+    <KineticGrid>
+          <PageShell>
+            <PlannerSection />
+          </PageShell>
+        </KineticGrid>
   );
 }
